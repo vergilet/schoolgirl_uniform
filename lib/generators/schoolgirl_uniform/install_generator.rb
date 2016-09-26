ce83ca95897ea83ed3adf362bdbc3f85ace2c355
YaroslavO <yaroslavo@softwareplanetgroup.com>
2016-09-26 12:54:40 +0300

    Done generators

--- lib/generators/schoolgirl_uniform/install_generator.rb
+++ lib/generators/schoolgirl_uniform/install_generator.rb
@@ -3,21 +3,21 @@
 
 module SchoolgirlUniform
   module Generators
-    class InstallGenerator < Rails::Generators::Base
+    class InstallGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
 
-      argument :controller_name, :type => :string, :default => "form"
-
-      def self.source_root
-        @_schoolgirl_uniform_source_root ||= File.expand_path("../templates", __FILE__)
-      end
+      argument :controller_name, type: :string, default: "form"
 
       def create_initializer_file
-        template "wizard.html.erb", "app/views/#{controller_name.underscore}/_wizard.html.erb"
-        template "show.html.erb", "app/views/#{controller_name.underscore}/show.html.erb"
-        template "steps/first_step.html.erb", "app/views/#{controller_name.underscore}/steps/_first_step.html.erb"
-        template "steps/last_step.html.erb", "app/views/#{controller_name.underscore}/steps/_last_step.html.erb"
-        template "forms/template_form.rb.erb", "app/forms/#{controller_name.underscore}_form.rb"
-        template "controllers/template_controller.rb.erb", "app/controllers/#{controller_name.underscore}_controller.rb"
+        puts controller_name.underscore
+        puts file_name
+
+        copy_file "wizard.html.erb", "app/views/#{file_name}/_wizard.html.erb"
+        copy_file "show.html.erb", "app/views/#{file_name}/show.html.erb"
+        copy_file "steps/first_step.html.erb", "app/views/#{file_name}/steps/_first_step.html.erb"
+        copy_file "steps/last_step.html.erb", "app/views/#{file_name}/steps/_last_step.html.erb"
+        template "forms/template_form.rb.erb", "app/forms/#{file_name}_form.rb"
+        template "controllers/template_controller.rb.erb", "app/controllers/#{file_name}_controller.rb"
       end
 
       def setup_routes
@@ -28,7 +28,7 @@ module SchoolgirlUniform
 
       def navigtation_resources
         <<-FILE
-resource :#{controller_name.underscore}, controller: '#{controller_name.underscore}', only: [:show, :create] do
+resource :#{file_name}, controller: '#{file_name}', only: [:show, :create] do
     collection do
       get  :current
       post :next
